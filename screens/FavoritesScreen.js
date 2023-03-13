66a21f18144c2e230d4576488bbf45d17223afa7
andrii.tantsiura <andrii.tantsiura@headworks.com.ua>
2023-03-13 17:56:12 +0200

    Lesson 118: the FavoriteScreen is completed

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,37 @@
-import { Text } from "react-native";
+import { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import MealsList from "../components/MealsList/MealsList";
+import { MEALS } from "../data/dummy-data";
+import { FavoritesContext } from "../store/context/favorites-context";
 
 function FavoritesScreen() {
-  return <Text>The favorites screen</Text>;
+  const favoritesContext = useContext(FavoritesContext);
+
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoritesContext.ids.includes(meal.id)
+  );
+
+  return favoriteMeals.length > 0 ? (
+    <MealsList items={favoriteMeals} />
+  ) : (
+    <View style={styles.rootContainer}>
+      <Text style={styles.text}>You have no favorite meals yet</Text>
+    </View>
+  );
 }
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
